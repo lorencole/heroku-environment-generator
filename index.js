183a72440f59f4fd2a5245611f1e4cc4bfaceef7
Loren Cole <loren@protea.cc>
2016-06-22 20:31:34 -0500

    Cleanup source option

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env node
 var exec = require('child_process').execSync;
 var fs = require("fs");
-//var prompt = require('prompt');
+
 var argv = require('yargs')
 			.usage('Usage: $0 --app [heroku_app_name] \n   or: source $0 --source')
 			.option('app', {
 				alias: 'a',
 				describe: 'Specify a heroku app`'
 			})
-			/*.option('source', {
-				alias: 's',
-				describe: 'Immediately export heroku environment variables into your shell. NOTE: to export environment variables you must call with \'source generate-env -source\''
-			})*/
 			.help('h')
 			.alias('h','help')
 			.argv;
@@ -55,19 +51,5 @@ var filename = argv.app ? argv.app : "heroku_env";
 filename += ".config";
 // TODO: Prompt if file already exists
 writeFile(filename, config);
-
-/* if not source write to a file */
-/*if(!argv.source) {
-	var filename = argv.app ? argv.app : "heroku_env";
-	filename += ".config";
-	// TODO: Prompt if file already exists
-	writeFile(filename, config);
-} else {
-	// write to the environment instead
-	config.forEach(function(env_var) {
-		process.stdout.write('\'' + env_var + '\'' + '\n');
-	})
-*/
-}
 
 //console.log(JSON.stringify(config, null, '\t'));
